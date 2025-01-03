826b28bd1bfcbbc95fba32bdff8b3b5e40ca4c80
nafis585 <princetoure@gmail.com>
2025-01-03 18:53:25 +0000

    nodemailer

---
+++ form.js
@@ -0,0 +1,28 @@
+var http = require('http');
+var url = require('url');
+var nodemailer = require('nodemailer');
+http.createServer(function(req, res){
+    res.writeHead(200);
+    q = url.parse(req.url, true).query;
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user:'toureprince513@gmail.com',
+            pass:'crew ehut ghfh agvr'
+        }
+    })
+    var mailoptions = {
+        from: 'toureprince513@gmail.com',
+        to: q.formulaire,
+        subject: 'un petit message pour toi!!',
+        text: 'hello handsome how are you?'
+    }
+    transporter.sendMail(mailoptions, function(error, info){
+        if (error){
+            res.end(error.message);
+        }
+        else{
+            res.end(info.res);
+        }
+    })
+}).listen(8080);
